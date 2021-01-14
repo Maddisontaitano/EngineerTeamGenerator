8c5035a573cdaf4371da32bee1894b02dad88770
Maddison Taitano <maddisontaitano@gmail.com>
2021-01-14 15:52:58 -0500

    added java for the app.js file

--- 02-Homework/Develop/app.js
+++ 02-Homework/Develop/app.js
@@ -18,6 +18,19 @@ const render = require("./lib/htmlRenderer");
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
 
+var employeeTitle = [
+    {
+      type: "list",
+      name: "role",
+      message: "Please select the role of the employee:?",
+      choices: [
+        "Engineer",
+        "Intern",
+        "Manager",
+        "Complete roster, no employees left to add.",
+      ],
+    },
+  ];
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
@@ -28,8 +41,143 @@ const render = require("./lib/htmlRenderer");
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+// Array for Intern questions
+var devInternQuest = [
+    {
+      type: "input",
+      message: "Please enter intern\'s name:",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "Please enter your intern\'s ID number:",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "Please enter intern\'s email address:",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "Please enter the school intern attends:",
+      name: "school",
+    },
+  ];
+  // Array for Engineer quesitons
+  var devEngineerQuest = [
+    {
+      type: "input",
+      message: "Please enter engineer\'s name:",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "Please enter engineer\'s ID number:",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "Please enter engineer\'s email address:",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "Please enter engineer\'s GitHub username:",
+      name: "github",
+    },
+  ];
+
+// Array for Manager questions
+  var devManagerQuest = [
+    {
+      type: "input",
+      message: "Please enter manager\'s name:",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "Please enter manager\'s ID number:",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "Please enter manager\'s email address:",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "Please enter manager\s office number:",
+      name: "officeNumber",
+    },
+  ];
+  
+  generateTeam();
+  
+// funtion that gives the user the right questions for whichever job title they selected
+  function generateTeam() {
+    inquirer.prompt(employeeTitle)
+      .then(function (response) {
+        console.log(response);
+        return response;
+      })
+      .then(function (response) {
+        // If user chooses Engineer, this will make sure that they are prompted with the right questions
+        if (response.role === 'Engineer') {
+          inquirer.prompt(devEngineerQuest).then((response) => {
+            console.log(response);
+  
+            var newEngineer = new Engineer(
+              response.name,
+              response.id,
+              response.email,
+              response.github
+            );
+            teamMembers.push(newEngineer);
+            console.log('A new engineer has been added to your team!');
+            generateTeam();
+          });
+  
+        // If user chooses Manager, this will make sure that they are prompted with the right questions
+    } else if (response.role === 'Manager') {
+          inquirer.prompt(devManagerQuest).then((response) => {
+            console.log(response);
+  
+            var newManager = new Manager(
+              response.name,
+              response.id,
+              response.email,
+              response.officeNumber
+            );
+            teamMembers.push(newManager);
+            console.log('A new manager has been added to your team!');
+            generateTeam();
+          });
+  
+        // If user chooses Intern, this will make sure that they are prompted with the right questions
+    } else if (response.role === 'Intern') {
+          inquirer.prompt(devInternQuest).then((response) => {
+            console.log(response);
+  
+            var newIntern = new Intern(
+              response.name,
+              response.id,
+              response.email,
+              response.school
+            );
+            teamMembers.push(newIntern);
+  
+            console.log('A new intern has been added to your team!');
+            generateTeam();
+          });
+  
+          // If user selects 'Engineer' as job title--prompt with questions for engineer
+        } else {
+          var main = render(teamMembers);
+          fs.writeFile(outputPath, main, (err) => {
+            if (err) throw err;
+            console.log('Your new team has been generated in the team.html file!')
+          });
+        }
+      });
+  }
